9a78ac97a45ca875afd274617ca29be46c37c87b
zhaosaisai <zhaosaisai@weidian.com>
2017-09-21 23:57:24 +0800

    修改脚本bug

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #/usr/bin/env bash
 
 # push ro origin 
-git add . && git commit -m "$1" && git push origin master && exit "push successfully" || exit "push failed"
+git add . && git commit -m "$1" && git push origin master >> git.log 2>&1 &&  exit 1  ||  exit 0 
